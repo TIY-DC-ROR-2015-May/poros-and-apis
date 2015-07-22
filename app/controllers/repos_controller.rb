6f6d2facf73a707e1f7fb3b572f091ddb6ad13d8
James Dabbs <jamesdabbs@gmail.com>
2015-07-22 17:15:07 -0400

    Extract logic to API object

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,9 +1,5 @@
 class ReposController < ApplicationController
   def display
-    response = GithubApi.get "/users/jamesdabbs/repos"
-    #@repositories = response.map { |r| r.values_at("name", "updated_at", "description") }
-    @repositories = response.map { |data|
-      Repository.new(data["name"], data["description"], data["updated_at"])
-    }
+    @repositories = GithubApi.new.repositories_for_user params[:username]
   end
 end
--- app/models/github_api.rb
+++ app/models/github_api.rb
@@ -1,4 +1,9 @@
 class GithubApi
   include HTTParty
   base_uri "https://api.github.com"
+
+  def repositories_for_user username
+    data = self.class.get("/users/#{username}/repos")
+    data.map { |h| Repository.new(h["name"], h["description"], h["updated_at"]) }
+  end
 end
